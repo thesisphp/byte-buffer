91dd3e6a147e638d422f5223ff5a93c5bc6a3992
kafkiansky <vadimzanfir@gmail.com>
2025-01-23 16:03:35 +0300

    Implement Flushable from thesis/byte-writer

--- src/BufferedReaderWriter.php
+++ src/BufferedReaderWriter.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Thesis\ByteBuffer;
 
 use Thesis\ByteReader\Reader;
+use Thesis\ByteWriter\Flushable;
 use Thesis\ByteWriter\Writer;
 
 /**
--- src/BufferedWriter.php
+++ src/BufferedWriter.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Thesis\ByteBuffer;
 
+use Thesis\ByteWriter\Flushable;
 use Thesis\ByteWriter\Writer;
 
 /**
